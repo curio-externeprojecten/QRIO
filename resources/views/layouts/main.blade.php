929c221a392ca9b44afb367d695478c536458909
ThijmenDekker <thijmen@visioen.nl>
2020-12-01 13:42:21 +0100

    the icon is now part of the main layout

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -7,7 +7,7 @@
     <title>QRIO</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <link rel="stylesheet" href="{{asset('resources/css/style.css')}}">
-
+    <link rel="icon" href="{{asset('resources/img/square_logo.png')}}">
 </head>
 <body style="position: relative; height: 100%; width: 100%; padding-bottom: 200px; margin: 0; min-height: 100%;">
     <header class="p-4 bg-dark text-light d-flex justify-content-around">
