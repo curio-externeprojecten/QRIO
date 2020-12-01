f19bf925eb6e6f6810b1f0d32c47b14913e58d48
ThijmenDekker <thijmen@visioen.nl>
2020-12-01 10:58:59 +0100

    changes to main layout

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,16 +1,17 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" style="height: 100%">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>QRIO</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{asset('resources/css/style.css')}}">
 
 </head>
-<body>
-    <header class="row p-4 bg-dark text-light">
-        <h1>QRIO</h1>
+<body style="min-height:100%">
+    <header class="row p-4 bg-dark text-light d-flex justify-content-around">
+        <img src="{{asset('resources/img/logo.png')}}" alt="">
         <nav class="m-2 p-1 float-right">
             <a href="" class="mr-3">Een link</a>
             <a href="" class="mr-3">Een link</a>
@@ -19,8 +20,8 @@
             <a href="" class="mr-3">Een link</a>
         </nav>
     </header>
-    <main>
-        <div class="container">
+    <main style="background:url({{asset('resources/img/background.png')}})">
+        <div class="container" style="background:rgba(245,245,245,0.7)">
             @yield("content")
         </div>
     </main>
