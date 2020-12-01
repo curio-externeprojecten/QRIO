2045cf13b1b350953dc0bbacca75ce2fcab5cf63
ThijmenDekker <thijmen@visioen.nl>
2020-12-01 10:21:00 +0100

    make beta version of de main layout

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>QRIO</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+</head>
+<body>
+    <header class="row p-4 bg-dark text-light">
+        <h1>QRIO</h1>
+        <nav class="m-2 p-1 float-right">
+            <a href="" class="mr-3">Een link</a>
+            <a href="" class="mr-3">Een link</a>
+            <a href="" class="mr-3">Een link</a>
+            <a href="" class="mr-3">Een link</a>
+            <a href="" class="mr-3">Een link</a>
+        </nav>
+    </header>
+    <main>
+        <div class="container">
+            @yield("content")
+        </div>
+    </main>
+    <footer>
+        <p>Made by CURIO students</p>
+    </footer>
+</body>
+</html>
