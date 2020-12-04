61955bd721c699ec9297f7695783b91e1eac999c
abdifatah <d272774@edu.rocwb.nl>
2020-12-04 10:18:48 +0100

    installed jetstream
    met livewire

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,7 @@ Route::get('/', function () {
 });
 
 Route::get('projects', [ProjectsController::class, 'index']);
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
