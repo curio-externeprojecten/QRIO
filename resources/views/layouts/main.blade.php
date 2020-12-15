00728d9d39c7c1679dadafe23a1a256a1d8780a1
abdifatah <d272774@edu.rocwb.nl>
2020-12-15 13:05:48 +0100

    qrio.test instead of qrio.me

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -18,8 +18,8 @@
             <a href="" class="mr-3">Een link</a>
             <a href="" class="mr-3">Een link</a>
             <a href="" class="mr-3">Een link</a>
-            <a href="http://qrio.me/register" class="mr-3">Register</a>
-            <a href="http://qrio.me/login" class="mr-3">Login</a>
+            <a href="http://qrio.test/register" class="mr-3">Register</a>
+            <a href="http://qrio.test/login" class="mr-3">Login</a>
         </nav>
         
             
