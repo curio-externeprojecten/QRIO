949cb3de6a7e97ddb185d7eafc6034dca6fafb26
ThijmenDekker <thijmen@visioen.nl>
2021-01-12 13:01:56 +0100

    new instruction button

--- resources/views/instructions/index.blade.php
+++ resources/views/instructions/index.blade.php
@@ -2,10 +2,11 @@
 
 @section('content')
     <h1>Instructies</h1>
+    <a href="{{ route('instructions.create') }}" class="btn btn-primary" style="margin-bottom:10px;">Nieuwe instructie</a>
     <ul class="list-group">
         @foreach ($instructions as $instruction)
             <li class="list-group-item">
-                <a href="{{route('instructions.show', $instruction->id)}}">{{ $instruction->name }} Created at: {{ $instruction->created_at }}</a>
+                <a href="{{route('instructions.show', $instruction->id)}}">{{ $instruction->project->name }} Created at: {{ $instruction->created_at }}</a>
             </li>
         @endforeach
     </ul>
