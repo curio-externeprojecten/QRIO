e01cd7d61448f3a983bf444fbad7d5caeeb358e8
ThijmenDekker <thijmen@visioen.nl>
2021-01-12 10:44:51 +0100

    removed random whitespace in show of instructions

--- resources/views/instructions/show.blade.php
+++ resources/views/instructions/show.blade.php
@@ -11,8 +11,7 @@
             <?php $input = str_replace("$image->code", "<br><img src=\"$image->file_path\" alt=\"$image->code\" style=\"width:200px;\"><br>",$input); ?>
         @else
             <?php $input = str_replace("$image->code", "<br><iframe width=\"420\" height=\"315\" src=\"$image->file_path\"> </iframe><br>",$input); ?>
-            @endif
-
+        @endif
     @endforeach
     <p>
     <?php echo $input ?>
