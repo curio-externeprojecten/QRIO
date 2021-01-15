71e7e7e5ea36232becf32288b6b0c86d19fa769b
BryanGrootscholten <bryangr@live.nl>
2021-01-15 11:29:26 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -23,14 +23,10 @@ Route::get('/', [MainController::class, 'index']);
 
 Route::get('qr-code', function () 
 {	
-	$qr = QRCode::text('QR Code Generator for Laravel!')->svg();
-	$file = 'public/resources/img/qr/' . $qr;
-	setOutfile($file);
-  	return $qr;    
-
-  	//	$file = asset('resources/img/qr/qrimage.svg');
+  	$file = asset('resources/img/qr/qrimage.svg');
 	
-//  	QRCode::text('QR Code Generator for Laravel!')->setOutfile($file);    
+	$qr = QRCode::text('QR Code Generator for Laravel!')->svg(); 
+	save($qr, $file);   
 
 });
 
